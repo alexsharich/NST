39152699095f85b0caca6d2838d4b6aceecb333d
alexsharich <alexsharich@gmail.com>
2026-03-02 22:24:52 +0300

    add user for sql dirty

--- src/modules/user-accounts/infrastructure/user.raw.repository.ts
+++ src/modules/user-accounts/infrastructure/user.raw.repository.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@nestjs/common';
-import {RegistrationConfirmationCode} from "../api/input-dto/registration-confirmation-code";
 import {PgService} from "../../../core/libs/pg/pg.service";
 
 @Injectable()
@@ -8,19 +7,91 @@ export class UsersRawRepository {
 
     }
 
-    async createUser(login:string,passwordHash:string,email:string) {
-        const res = await this.pgService.runQuery<{id:string}>(`
+    async createUser(login: string, passwordHash: string, email: string) {
+        const res = await this.pgService.runQuery<{ id: string }>(`
             INSERT INTO users (login, "passwordHash", email)
-            VALUES ($1, $2, $3) 
-            RETURNING id
-        `, [login,passwordHash,email])
-        const userId = res?.rows?.[0].id
-        return userId
+            VALUES ($1, $2, $3) RETURNING id
+        `, [login, passwordHash, email])
+        return res?.rows?.[0].id // return userId
     }
+
+    async findOneSQL(userId: string) {
+        const res = await this.pgService.runQuery<{ id: string, login: string, email: string, createdAt: any }>(
+            `
+                SELECT id, login, email, "createdAt"
+                FROM users
+                WHERE id = $1
+                /*RETURNING id, login, email, "createdAt"*/
+            `, [userId]
+        )
+        return res?.rows?.[0]
+    }
+
+    async findUserByLoginSQL(login: string) {
+        const res = await this.pgService.runQuery<{ id: string, login: string, email: string, createdAt: any }>(
+            `
+                SELECT id, login, email, "createdAt"
+                FROM users
+                WHERE login = $1
+                /*RETURNING id, login, email, "createdAt"*/
+            `, [login]
+        )
+        return res?.rows?.[0]
+    }
+
+    async findUserByLoginOrEmailSQL(loginOrEmail: string) {
+        const res = await this.pgService.runQuery<{ id: string, login: string, email: string, createdAt: any }>(
+            `
+                SELECT id, login, email, "createdAt"
+                FROM users
+                WHERE login = $1
+                   OR email = $1
+                /*RETURNING id, login, email, "createdAt"*/
+            `, [loginOrEmail]
+        )
+        return res?.rows?.[0]
+    }
+
+    async findUserByEmailSQL(email: string) {
+        const res = await this.pgService.runQuery<{ id: string, login: string, email: string, createdAt: any }>(
+            `
+                SELECT id, login, email, "createdAt"
+                FROM users
+                WHERE email = $1
+                /*RETURNING id, login, email, "createdAt"*/
+            `, [email]
+        )
+        return res?.rows?.[0]
+    }
+
+    async findUserByConfirmationCodeSQL(confirmationCode: string) {
+        const res = await this.pgService.runQuery<{ id: string, login: string, email: string, createdAt: any }>(
+            `
+                SELECT id, login, email, "createdAt"
+                FROM users
+                WHERE "emailConfirmation"."confirmationCode" = $1
+                /*RETURNING id, login, email, "createdAt"*/
+            `, [confirmationCode]
+        )
+        return res?.rows?.[0]
+    }
+
+    async findUserByRecoveryCodeSQL(recoveryCode: string) {
+        const res = await this.pgService.runQuery<{ id: string, login: string, email: string, createdAt: any }>(
+            `
+                SELECT id, login, email, "createdAt"
+                FROM users
+                WHERE "passwordRecovery"."recoveryCode" = $1
+                /*RETURNING id, login, email, "createdAt"*/
+            `, [recoveryCode]
+        )
+        return res?.rows?.[0]
+    }
+
 
     /*async findOne(id: string) {
         return this.userModel.findOne({_id: id, deletedAt: null})
-    }
+    } //done
 
     async findUserByLogin(login: string) {
         return this.userModel.findOne({login, deletedAt: null})
